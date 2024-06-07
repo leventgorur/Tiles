da4fa7e71322f71ff555c75c9b9443248ecb93ba
leventgorur <swgorur@gmail.com>
2024-06-07 11:42:53 -0400

    first commit

---
+++ layout_tile.py
@@ -0,0 +1,10 @@
+ROOM_SIZE = [69.5, 73]
+GROUT_SIZE = 0.25
+TOLERANCE_X = 0.25
+TOLERANCE_Y = 0.25
+PACKAGE_TILES = {(A:2),(B:2),(C:1),(D:2)}
+A = {(16, 24)}
+B = {(16, 16)}
+C = {(8, 16)}
+D = {(8, 8)}
+
